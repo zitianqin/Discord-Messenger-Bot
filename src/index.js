47616506e56aeada80541ca0e1baf0aec0c9f956
Zitian Qin <z5476244@ad.unsw.edu.au>
2024-02-10 13:47:32 +1100

    Remove failure responses to user

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable brace-style */
+/* eslint-disable spaced-comment */
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
@@ -16,18 +18,18 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
-  }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 
@@ -38,16 +40,16 @@ for (const folder of commandFolders) {
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-  // Send the reminders out at the start of every minute.
-  const currentDate = new Date();
-  const currentSecond = currentDate.getSeconds() * 1000;
-  const currentMillisecond = currentDate.getMilliseconds();
-  const timeToWait = 60000 - (currentSecond + currentMillisecond);
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+	// Send the reminders out at the start of every minute.
+	const currentDate = new Date();
+	const currentSecond = currentDate.getSeconds() * 1000;
+	const currentMillisecond = currentDate.getMilliseconds();
+	const timeToWait = 60000 - (currentSecond + currentMillisecond);
 
-  sendScheduledMessages(client);
+	sendScheduledMessages(client);
 
-  setTimeout(setInterval, timeToWait, () => sendScheduledMessages(client), 60000);
+	setTimeout(setInterval, timeToWait, () => sendScheduledMessages(client), 60000);
 });
 
 // Log in to Discord with your client's token
@@ -59,23 +61,18 @@ client.login(token);
 ///////////////////////////////////////////////////////////////////////////////
 
 client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
+	const command = interaction.client.commands.get(interaction.commandName);
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-    } else {
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-  }
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+	}
 });
